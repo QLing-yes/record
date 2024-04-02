4623e2d3d4ec5d7795a87f80caa81d7b03ce3eca
QLing-yes <78684352+QLing-yes@users.noreply.github.com>
2024-04-02 09:47:13 +0800

    Create config.ts

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,35 @@
+import { DefaultTheme, defineConfig } from 'vitepress';
+import pressAuto from "./vitepress-auto";
+
+let dirItems = []
+
+// https://vitepress.yiov.top/plugin.html
+// https://vitepress.dev/zh/reference/site-config
+export default defineConfig({
+  lang: 'zh-CN',
+  title: 'Record',
+  description: 'VitePress',
+  base: '/',
+  cleanUrls: true,
+  ignoreDeadLinks: true,
+  metaChunk: true,
+  // mpa: true,
+  lastUpdated: true,
+
+  themeConfig: {
+    search: {
+      provider: 'local',
+    },
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/QLing-yes' },
+    ],
+    nav: [],
+    sidebar: pressAuto({
+      path: '/notes',
+    })
+  },
+  vite: {},
+  vue: {
+    // @vitejs/plugin-vue 选项
+  }
+});
